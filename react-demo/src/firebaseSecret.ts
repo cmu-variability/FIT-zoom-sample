bf9bcd57a666dba33ad7f5bf012538dac3d628ef
Aditya Ganesh <adityawolverines@gmail.com>
2024-01-29 12:27:34 -0500

    adding gitignore change

---
+++ react-demo/src/firebaseSecret.ts
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBw-Xyyr4Zp_QPATN-ONnxx73c0zVzboZM",
+    authDomain: "fit-react-6fefb.firebaseapp.com",
+    projectId: "fit-react-6fefb",
+    storageBucket: "fit-react-6fefb.appspot.com",
+    messagingSenderId: "547256975599",
+    appId: "1:547256975599:web:3fac54a43a5a666bf19b1a",
+    measurementId: "G-N80PSJQDCX"
+};
+
+export default firebaseConfig;
